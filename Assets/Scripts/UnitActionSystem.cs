058f5bc0d529ce9b07da5bae7f07d7858f8ccf3c
Nathan Gagne <nathangagne.dev@gmail.com>
2023-06-23 14:06:39 -0500

    Implement logic for selected visual

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,17 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    //Singleton
+    public static UnitActionSystem Instance { get; private set; }
+
+    public event EventHandler OnSelectedUnitChanged;
+
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayer;
 
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There is more than one UnitActionSystem! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
     private void Update()
     {
-        if (!HandleUnitSelection())
+        if (Input.GetMouseButtonDown(0))
         {
-            if (Input.GetMouseButton(0))
+            if (!HandleUnitSelection())
             {
                 selectedUnit.Move(MouseWorld.GetPosition());
             }  
@@ -21,20 +38,31 @@ public class UnitActionSystem : MonoBehaviour
     private bool HandleUnitSelection()
     {
         bool pickedUnit = false;
-        if (Input.GetMouseButton(0))
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);   
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitLayer))
+        if (raycastHit.collider != null)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);   
-            if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitLayer))
-            if (raycastHit.collider != null)
+            if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
             {
-                if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
-                {
-                    selectedUnit = unit;
-                    pickedUnit = true;
-                }
+                SetSelectedUnit(unit);
+                pickedUnit = true;
             }
         }
 
         return pickedUnit;
+    }
+
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+
+        //Checks if the event is null (no subscribers), then proceeds with Invoke if not null
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
     }
 }
---
+++ Assets/Scripts/UnitSelectedVisual.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelectedVisual : MonoBehaviour
+{
+    [SerializeField] private Unit unit;
+
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+    }
+
+    private void Start()
+    {
+        UnitActionSystem.Instance.OnSelectedUnitChanged += UnitActionSystem_OnSelectedUnitChanged;
+
+        UpdateVisual();
+    }
+
+    private void UnitActionSystem_OnSelectedUnitChanged(object sender, EventArgs empty)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        meshRenderer.enabled = UnitActionSystem.Instance.GetSelectedUnit() == unit ? true : false;
+    }
+}
