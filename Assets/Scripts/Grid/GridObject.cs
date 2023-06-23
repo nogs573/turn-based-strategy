66259c8e12a24044c591a44de678734f2cb89691
Nathan Gagne <nathangagne.dev@gmail.com>
2023-06-23 15:14:11 -0500

    Set up GridSystem with debug visuals

---
+++ Assets/Scripts/Grid/GridObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridObject
+{
+    private GridPosition gridPosition;
+    private GridSystem gridSystem;
+
+    public GridObject(GridSystem gridSystem, GridPosition gridPosition)
+    {
+        this.gridSystem = gridSystem;
+        this.gridPosition = gridPosition;
+    }
+
+    public override string ToString()
+    {
+        return gridPosition.ToString();
+    }
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private Transform gridDebugObjectPrefab;
+
+    private GridSystem gridSystem;
+
+    private void Start()
+    {
+        gridSystem = new GridSystem(10, 10, 2f);
+        gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
+    }
+
+    private void Update()
+    {
+        Debug.Log(gridSystem.GetGridPosition(MouseWorld.GetPosition()));
+    }
+}
