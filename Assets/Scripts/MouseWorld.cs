7f312bc35570183a4541921dcc26b881c2556639
Nathan Gagne <nathangagne.dev@gmail.com>
2023-06-23 11:47:07 -0500

    Add mouse raycast logic

---
+++ Assets/Scripts/MouseWorld.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+    private static MouseWorld instance;
+
+    [SerializeField] private LayerMask mousePlaneMask;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        transform.position = MouseWorld.GetPosition();
+    }
+
+    public static Vector3 GetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);   
+        Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, instance.mousePlaneMask);
+        return raycastHit.point;
+    }
+}
