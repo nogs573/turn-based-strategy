805e026efeef7e9fccc6845db7dae80b0f3bf6bb
Nathan Gagne <nathangagne.dev@gmail.com>
2023-06-23 22:36:21 -0500

    Implement valid grid position visual

--- Assets/Scripts/Grid/GridSystemVisual.cs
+++ Assets/Scripts/Grid/GridSystemVisual.cs
@@ -4,15 +4,75 @@ using UnityEngine;
 
 public class GridSystemVisual : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	public static GridSystemVisual Instance { get; private set; }
+	[SerializeField] private Transform gridSystemVisualSinglePrefab;
+
+	private GridSystemVisualSingle[,] gridSystemVisualSingleArray;
+
+	private int width;
+	private int height;
+
+	private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Debug.LogError("There is more than one GridSystemVisual! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private void Start()
+	{
+		width = LevelGrid.Instance.GetWidth();
+		height = LevelGrid.Instance.GetHeight();
+
+		gridSystemVisualSingleArray = new GridSystemVisualSingle[width,height];
+		
+		for (int x = 0; x < width; x++)
+		{
+			for (int z = 0; z < height; z++)
+			{
+				GridPosition gridPosition = new GridPosition(x, z);
+				Transform gridSystemVisualSingleTransform =  Instantiate(gridSystemVisualSinglePrefab, LevelGrid.Instance.GetWorldPosition(gridPosition), Quaternion.identity);
+
+				gridSystemVisualSingleArray[x,z] = gridSystemVisualSingleTransform.GetComponent<GridSystemVisualSingle>();
+			}
+		}
+	}
+
+	private void Update()
+	{
+		UpdateGridVisual();
+	}
+
+	public void HideAllGridPositions()
+	{
+		for (int x = 0; x < width; x++)
+		{
+			for (int z = 0; z < height; z++)
+			{
+				gridSystemVisualSingleArray[x,z].Hide();
+			}
+		}
+
+	}
+
+	public void ShowGridPositionList(List<GridPosition> gridPositionList)
+	{
+		foreach (GridPosition gridPosition in gridPositionList)
+		{
+			gridSystemVisualSingleArray[gridPosition.x, gridPosition.z].Show();
+		}
+	}
+
+	private void UpdateGridVisual()
+	{
+		HideAllGridPositions();
+
+		Unit selectedUnit = UnitActionSystem.Instance.GetSelectedUnit();
+
+		ShowGridPositionList(selectedUnit.GetMoveAction().GetValidActionGridList());
+	}
 }
--- Assets/Scripts/Grid/Testing.cs
+++ Assets/Scripts/Grid/Testing.cs
@@ -8,15 +8,15 @@ public class Testing : MonoBehaviour
     [SerializeField] private Unit unit;
     private void Start()
     {
-        // gridSystem = new GridSystem(10, 10, 2f);
-        // gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
+        
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            unit.GetMoveAction().GetValidActionGridList();
+            GridSystemVisual.Instance.HideAllGridPositions();
+            GridSystemVisual.Instance.ShowGridPositionList(unit.GetMoveAction().GetValidActionGridList());                        
         }
     }
 }
