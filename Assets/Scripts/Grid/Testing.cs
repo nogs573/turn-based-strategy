e85338fffaa8216729b4c1faea600ea07b49b92e
Nathan Gagne <nathangagne.dev@gmail.com>
2023-06-25 18:11:25 -0500

    Implement A* pathfinding

--- Assets/Scripts/Grid/Testing.cs
+++ Assets/Scripts/Grid/Testing.cs
@@ -13,6 +13,18 @@ public class Testing : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            GridPosition mouseGridPosition = LevelGrid.Instance.GetGridPosition(MouseWorld.GetPosition());
+            GridPosition startGridPosition = new GridPosition(0, 0);
+
+            List<GridPosition> gridPath = Pathfinding.Instance.FindPath(startGridPosition, mouseGridPosition);
+
+            for (int i = 0; i < gridPath.Count - 1; i++)
+            {
+                Debug.DrawLine(LevelGrid.Instance.GetWorldPosition(gridPath[i]), LevelGrid.Instance.GetWorldPosition(gridPath[i + 1]), Color.white, 10f);
+            }
+        }
     
     }
 }
